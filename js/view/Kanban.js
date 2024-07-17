e6d40fed5f9b6f87935fbdbbf08a0fba9f4b0fcf
Andranik Virabyan <gmparstone99@gmail.com>
2024-07-17 12:08:58 -0800

    Working code

--- js/view/Kanban.js
+++ js/view/Kanban.js
@@ -1,31 +1,20 @@
-import Column from './Column.js'
+import Column from "./Column.js";
 
 export default class Kanban {
-  constructor(root) {
-    this.root = root
+    constructor(root) {
+        this.root = root;
 
-    Kanban.columns().forEach((column) => {
-      //TODO create an instance Column class
-      const columnView = new Column(column.id, column.title)
+        Kanban.columns().forEach(column => {
+            const columnView = new Column(column.id, column.title);
+            this.root.appendChild(columnView.elements.root);
+        });
+    }
 
-      this.root.appendChild(columnView.elements.root)
-    })
-  }
-
-  static columns() {
-    return [
-      {
-        id: 1,
-        title: 'Not Started',
-      },
-      {
-        id: 2,
-        title: 'In Progress',
-      },
-      {
-        id: 3,
-        title: 'Completed',
-      },
-    ]
-  }
+    static columns() {
+        return [
+            { id: 1, title: "Not Started" },
+            { id: 2, title: "In Progress" },
+            { id: 3, title: "Completed" }
+        ];
+    }
 }
